3b2e82e3525b78769c028ff0f3e85a9e6f3de144
szvitek <gy.szvitek@gmail.com>
2024-04-14 23:00:27 +0100

    init(Components): components added, css, react-icons

---
+++ src/components/Background.tsx
@@ -0,0 +1,7 @@
+export default function Background() {
+  return (
+    <div className="background">
+      <img src="pattern.svg" alt="Background pattern" />
+    </div>
+  );
+}
---
+++ src/components/Logo.tsx
@@ -0,0 +1,7 @@
+export default function Logo() {
+  return (
+    <a href="." className="logo">
+      <img src="logo.svg" alt="Logo" className="logo__img" />
+    </a>
+  );
+}
