cdbfd0efcff4b3a70e58e5516e276b95c422a6dd
szvitek <gy.szvitek@gmail.com>
2024-04-15 02:28:22 +0100

    feat(SearchForm): fetch data

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,7 +1,26 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function SearchForm() {
   const [searchText, setSearchText] = useState('');
+  const [jobItems, setJobItems] = useState([]);
+
+  useEffect(() => {
+    if (!searchText) return;
+
+    // react recommends to do data fetching in the event handler
+    // if it's related to some event just in our case
+    // but we do it like this because it will be  refactored later anyway
+    const fetchData = async () => {
+      const response = await fetch(
+        `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`
+      );
+      const data = await response.json();
+      setJobItems(data.jobItems);
+    };
+
+    fetchData();
+  }, [searchText]);
+
   return (
     <form
       onSubmit={(e) => {
