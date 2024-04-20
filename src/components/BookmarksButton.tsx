389c02d39536dc04709c068e3f4ae0a0a8d186d0
szvitek <gy.szvitek@gmail.com>
2024-04-20 11:57:58 +0100

    feat(PopOver): click outside - react way with refs

--- src/components/BookmarksButton.tsx
+++ src/components/BookmarksButton.tsx
@@ -1,39 +1,41 @@
 import { TriangleDownIcon } from '@radix-ui/react-icons';
 import BookmarksPopover from './BookmarksPopover';
-import { useEffect, useState } from 'react';
+import { ElementRef, useEffect, useRef, useState } from 'react';
 
 export default function BookmarksButton() {
   const [isOpen, setIsOpen] = useState(false);
+  const buttonRef = useRef<ElementRef<'button'>>(null);
+  const popoverRef = useRef<ElementRef<'div'>>(null);
 
   useEffect(() => {
     const handleClick = (e: MouseEvent) => {
       if (
         e.target instanceof HTMLElement &&
-        !e.target.closest('.bookmarks-btn') &&
-        !e.target.closest('.bookmarks-popover')
+        !buttonRef.current?.contains(e.target) &&
+        !popoverRef.current?.contains(e.target)
       ) {
         setIsOpen(false);
       }
-    }
-  
+    };
+
     document.addEventListener('click', handleClick);
 
     return () => {
       document.removeEventListener('click', handleClick);
-    }
-  }, [])
-  
+    };
+  }, []);
 
   return (
     <section>
       <button
         onClick={() => setIsOpen((prev) => !prev)}
         className="bookmarks-btn"
+        ref={buttonRef}
       >
         Bookmarks <TriangleDownIcon />
       </button>
 
-      {isOpen && <BookmarksPopover />}
+      {isOpen && <BookmarksPopover ref={popoverRef} />}
     </section>
   );
 }
--- src/components/BookmarksPopover.tsx
+++ src/components/BookmarksPopover.tsx
@@ -1,12 +1,20 @@
-import { useBookmarksContext } from "../lib/hooks";
-import JobList from "./JobList";
+import { ElementRef, forwardRef } from 'react';
+import { useBookmarksContext } from '../lib/hooks';
+import JobList from './JobList';
 
-export default function BookmarksPopover() {
+/*
+forwardRef<RefType, PropsType>(Component)
+and props and ref will be typed properly
+*/
+
+const BookmarksPopover = forwardRef<ElementRef<'div'>>(function (_props, ref) {
   const { bookmarkedJobItems, isLoading } = useBookmarksContext();
 
   return (
-    <div className="bookmarks-popover">
+    <div className="bookmarks-popover" ref={ref}>
       <JobList jobItems={bookmarkedJobItems} isLoading={isLoading} />
     </div>
   );
-}
+});
+
+export default BookmarksPopover;
