e10a3a61008ee403b747f495067336c60c29926f
szvitek <gy.szvitek@gmail.com>
2024-04-20 12:18:10 +0100

    feat(PopOver): create portal for popover

--- src/components/BookmarksPopover.tsx
+++ src/components/BookmarksPopover.tsx
@@ -1,6 +1,7 @@
 import { ElementRef, forwardRef } from 'react';
 import { useBookmarksContext } from '../lib/hooks';
 import JobList from './JobList';
+import { createPortal } from 'react-dom';
 
 /*
 forwardRef<RefType, PropsType>(Component)
@@ -10,10 +11,11 @@ and props and ref will be typed properly
 const BookmarksPopover = forwardRef<ElementRef<'div'>>(function (_props, ref) {
   const { bookmarkedJobItems, isLoading } = useBookmarksContext();
 
-  return (
+  return createPortal(
     <div className="bookmarks-popover" ref={ref}>
       <JobList jobItems={bookmarkedJobItems} isLoading={isLoading} />
-    </div>
+    </div>,
+    document.body
   );
 });
 
